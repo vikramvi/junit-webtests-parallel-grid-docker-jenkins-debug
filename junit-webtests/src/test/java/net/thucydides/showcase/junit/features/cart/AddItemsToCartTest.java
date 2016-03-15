36bf57a3512b57b59ca88a0f4f4e01e99a9ad804
vikramvi <vikram.ingleshwar@yahoo.com>
2016-03-15 13:08:59 +0100

    added sample google test case to check on Linux VM Docker container related issue https://github.com/serenity-bdd/serenity-core/issues/353

--- junit-webtests/src/test/java/net/thucydides/showcase/junit/features/cart/AddItemsToCartTest.java
+++ junit-webtests/src/test/java/net/thucydides/showcase/junit/features/cart/AddItemsToCartTest.java
@@ -1,15 +1,21 @@
 package net.thucydides.showcase.junit.features.cart;
 
+import java.util.concurrent.TimeUnit;
+
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.annotations.WithTag;
 import net.thucydides.core.annotations.WithTags;
+import net.thucydides.core.annotations.findby.By;
 import net.thucydides.showcase.junit.model.ListingItem;
 import net.thucydides.showcase.junit.steps.serenity.BuyerSteps;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 @RunWith(SerenityRunner.class)
 public class AddItemsToCartTest {
@@ -33,6 +39,17 @@ public class AddItemsToCartTest {
         // THEN
         buyer.should_see_item_in_cart(selectedItem);
         buyer.should_see_total_including_shipping_for(selectedItem);
+    }
+    
+    @Test
+    public void google_test(){
+    	WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://www.google.com");
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("ChromeDriver");
+        element.submit();
+        driver.close();
     }
 }
 
